ebade76421e50d313f9d91c74ff4e8c17566dc6f
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-06-30 21:49:56 +0300

    added api documentation openapi and swagger

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework.schemas import get_schema_view
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blog API",
+        default_version="v1",
+        description="A sample API for learning DRF",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="hello@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny,],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +42,11 @@ urlpatterns = [
     path('api-auth/', include("rest_framework.urls")),
     path('api/v1/dj-rest-auth/', include("dj_rest_auth.urls")),
     path('api/v1/dj-rest-auth/registration/',
-         include('dj_rest_auth.registration.urls'))
+         include('dj_rest_auth.registration.urls')),
+    path('swagger/', schema_view.with_ui(  # new
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui(  # new
+        'redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
