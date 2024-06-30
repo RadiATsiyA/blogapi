c08596051ccb397981c81a11a376d47338fb1251
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-06-30 21:47:38 +0300

    changed views to viewsets

--- posts/views.py
+++ posts/views.py
@@ -1,26 +1,16 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from django.contrib.auth import get_user_model
 from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 from .models import Post
 
 
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class UserList(generics.ListCreateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
